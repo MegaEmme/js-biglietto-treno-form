b69b9dc3a61990f1d6d26d24ec7468cb34c16173
MegaEmme <mikmusco@gmail.com>
2025-03-05 15:32:55 +0100

    feat: first attempt

--- script.js
+++ script.js
@@ -1,48 +1,66 @@
+const userDistance = prompt('ins');//document.getElementById('km');
+console.log(parseInt(userDistance));
+const finalPrice = userDistance * 0.21;
+console.log(finalPrice.toFixed(2));
+const userAge = prompt('age');//document.getElementById('age');
+console.log(parseInt(userAge));
+const generate = document.getElementById('button');
+console.log(generate);
+
+generate.addEventListener('click',function(){
+    console.log(`hai ${userAge} anni e il biglietto costa ${finalPrice}`);
+})
+
+
+
+
+
+
+
 // Chiedo all'utente il *numero di km* e lo salvo in una variabile
-const kmDistance = prompt ('Inserisci la distanza in km');
-console.log(parseInt(kmDistance))
+// const kmDistance = prompt ('Inserisci la distanza in km');
+// console.log(parseInt(kmDistance))
 
-// Chiedo all'utente l'*età* e la salvo in una variabile
-const age = prompt('Inserisci la tua età');
-console.log(parseInt(age))
+// // Chiedo all'utente l'*età* e la salvo in una variabile
+// const age = prompt('Inserisci la tua età');
+// console.log(parseInt(age))
 
 
-// Calcolo il *prezzo del biglietto* e lo salvo il una variabile
-const totalPrice = (kmDistance * 0.21)
-console.log(totalPrice)
+// // Calcolo il *prezzo del biglietto* e lo salvo il una variabile
+// const totalPrice = (kmDistance * 0.21)
+// console.log(totalPrice)
 
 
 // **Se** l'età è minore di 18 anni:
 //            - applico il 20% di sconto;
 //            - mostro il prezzo.
-if (age < 18) {
+// if (age < 18) {
 
-    const discountTwenty = (totalPrice / 100) * 80
-    console.log(`Hai meno di 18 anni
-        il prezzo scontato del 20% è di: ${(discountTwenty.toFixed(2))} €`)
-    alert(`Hai meno di 18 anni
+//     const discountTwenty = (totalPrice / 100) * 80
+//     console.log(`Hai meno di 18 anni
+//         il prezzo scontato del 20% è di: ${(discountTwenty.toFixed(2))} €`)
+//     alert(`Hai meno di 18 anni
         
-        - Il prezzo scontato del 20% è di: ${discountTwenty.toFixed(2)} €`)
+//         - Il prezzo scontato del 20% è di: ${discountTwenty.toFixed(2)} €`)
 
-// **Altrimenti se** l'età è maggiore di 65 anni:
-//            - applico il 40% di sconto;
-//            - mostro il prezzo.
-} else if (age >= 65) {
+// // **Altrimenti se** l'età è maggiore di 65 anni:
+// //            - applico il 40% di sconto;
+// //            - mostro il prezzo.
+// } else if (age >= 65) {
 
-    const discountForty = ((totalPrice /100) * 60)
-    console.log(`Hai più di 65 anni
-        Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`)
-    alert(`Hai più di 65 anni:
+//     const discountForty = ((totalPrice /100) * 60)
+//     console.log(`Hai più di 65 anni
+//         Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`)
+//     alert(`Hai più di 65 anni:
 
-        - Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`)
+//         - Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`)
 
-// **Altrimenti**:
-//             - mostro il prezzo.
-} else {
+// // **Altrimenti**:
+// //             - mostro il prezzo.
+// } else {
 
-    console.log(`Il prezzo del biglietto è di: ${totalPrice.toFixed(2)} €`)
-    alert(`Il prezzo del biglietto è di: ${totalPrice.toFixed(2)} €`)
+//     console.log(`Il prezzo del biglietto è di: ${totalPrice.toFixed(2)} €`)
+//     alert(`Il prezzo del biglietto è di: ${totalPrice.toFixed(2)} €`)
     
-}
-
+// }
 
