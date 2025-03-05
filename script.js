99fef1b7712e3b6fa8d0d3c6dbb1ad7471f1693b
MegaEmme <mikmusco@gmail.com>
2025-03-05 17:02:44 +0100

    solved milestone 1

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const userDistance = prompt('ins');//document.getElementById('km');
-console.log(parseInt(userDistance));
-const finalPrice = userDistance * 0.21;
-console.log(finalPrice.toFixed(2));
-const userAge = prompt('age');//document.getElementById('age');
-console.log(parseInt(userAge));
-const generate = document.getElementById('button');
-console.log(generate);
+const usernameElement = document.getElementById('username');
+console.log(usernameElement);
 
-generate.addEventListener('click',function(){
-    console.log(`hai ${userAge} anni e il biglietto costa ${finalPrice}`);
+const distanceInputElement = document.getElementById('km');
+console.log(distanceInputElement);
+
+const ageInputElement = document.getElementById('age');
+console.log(ageInputElement);
+
+const submitButton = document.getElementById('button');
+console.log(submitButton);
+
+submitButton.addEventListener('click',function(){
+
+    const finalPrice = distanceInputElement.value * 0.21;
+    if (ageInputElement.value < 18) {
+
+        const discountTwenty = (finalPrice / 100) * 80
+        console.log(`Ciao ${usernameElement.value} 
+            Hai meno di 18 anni
+            il prezzo scontato del 20% è di: ${(discountTwenty.toFixed(2))} €`);
+    
+        } else if (ageInputElement.value >= 65) {
+    
+        const discountForty = ((finalPrice /100) * 60)
+        console.log(`Ciao ${usernameElement.value} 
+            Hai più di 65 anni
+            Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`);
+    
+        } else {
+    
+        console.log(`Ciao ${usernameElement.value} 
+            Non hai diritto a sconti
+            Il prezzo del biglietto è di: ${finalPrice.toFixed(2)} €`);
+        
+    }
 })
 
 
@@ -17,50 +42,5 @@ generate.addEventListener('click',function(){
 
 
 
-// Chiedo all'utente il *numero di km* e lo salvo in una variabile
-// const kmDistance = prompt ('Inserisci la distanza in km');
-// console.log(parseInt(kmDistance))
 
-// // Chiedo all'utente l'*età* e la salvo in una variabile
-// const age = prompt('Inserisci la tua età');
-// console.log(parseInt(age))
-
-
-// // Calcolo il *prezzo del biglietto* e lo salvo il una variabile
-// const totalPrice = (kmDistance * 0.21)
-// console.log(totalPrice)
-
-
-// **Se** l'età è minore di 18 anni:
-//            - applico il 20% di sconto;
-//            - mostro il prezzo.
-// if (age < 18) {
-
-//     const discountTwenty = (totalPrice / 100) * 80
-//     console.log(`Hai meno di 18 anni
-//         il prezzo scontato del 20% è di: ${(discountTwenty.toFixed(2))} €`)
-//     alert(`Hai meno di 18 anni
-        
-//         - Il prezzo scontato del 20% è di: ${discountTwenty.toFixed(2)} €`)
-
-// // **Altrimenti se** l'età è maggiore di 65 anni:
-// //            - applico il 40% di sconto;
-// //            - mostro il prezzo.
-// } else if (age >= 65) {
-
-//     const discountForty = ((totalPrice /100) * 60)
-//     console.log(`Hai più di 65 anni
-//         Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`)
-//     alert(`Hai più di 65 anni:
-
-//         - Il prezzo scontato del 40% è di: ${discountForty.toFixed(2)} €`)
-
-// // **Altrimenti**:
-// //             - mostro il prezzo.
-// } else {
-
-//     console.log(`Il prezzo del biglietto è di: ${totalPrice.toFixed(2)} €`)
-//     alert(`Il prezzo del biglietto è di: ${totalPrice.toFixed(2)} €`)
-    
-// }
 
